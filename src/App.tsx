1d40afcbc27c12f7242851f8f43d2d7cc3a57722
KiritoKing <kiritoclzh@gmail.com>
2022-09-28 17:42:22 +0800

    finish upload basic logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import { Box } from '@mui/material';
+import Input from '@mui/material/Input';
+import Button from '@mui/material/Button';
 
 function App() {
+  const [file, setFile] = useState<File>();
+
+  let uploadHandler = () => {
+    console.log("State:" + file);
+    if(file != undefined) {
+      const data = new FormData();
+      data.append('file', file);
+      let promise = fetch('http://localhost:5000/upload', {
+        method: 'POST',
+        body: data
+      }).then(response => console.log(response))  
+    }
+    else {
+      console.error("No valid file loaded!");
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box component="form">
+        <div className='FileInput'>
+          <Input id='FileInput' type='file' onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            console.log(e.target.files);
+            if(e.target != null) {
+              setFile(e.target.files![0]);
+            }
+          }} />
+          <Button onClick={uploadHandler}>Upload</Button>
+        </div>
+      </Box>
     </div>
   );
 }
