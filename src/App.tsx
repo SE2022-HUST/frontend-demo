260ac76f19bab3c08ccf4db0997542820095b663
KiritoKing <kiritoclzh@gmail.com>
2022-09-29 18:49:57 +0800

    component split done

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
 import React, {useState, useRef} from 'react';
 import { Box } from '@mui/material';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import { Delete } from '@mui/icons-material';
-import Input from '@mui/material/Input';
-
 import './App.css';
-
+import FileInput from './Components/FileInput/FileInput';
+import ControlPanel from './Components/ControlPanel/ControlPanel';
 import uploadFunc from './Apis/Upload';
 import { uploadAddr } from './Apis/Constants';
-import FileInput from './Components/FileInput/FileInput';
-import DisplayBox from './Components/DisplayBox/DisplayBox';
 
 function App() {
   const [file, setFile] = useState<File>();
   const inputRef = useRef<HTMLInputElement>(null);
   let response: Response | undefined;
-
-  let uploadHandler = () => {
+  const uploadHandler = () => {
     console.log(file);
     response = uploadFunc(uploadAddr, file);
+  }
+  const openHandler = () => {
+    if(inputRef.current != null) {
+      inputRef.current.click();
+    }
+  }
+  const clearHandler = () => {
+    setFile(undefined);
   }
 
   return (
@@ -27,28 +28,10 @@ function App() {
       <div className='mainBar'>
         <Box component="form">
           <FileInput action={(file: File) => setFile(file)} ref={inputRef} />
-          <div className='ManuButtons'>
-            <DisplayBox file={file} />
-            <span className='Button'>
-              <Button variant='contained' onClick={() => {
-                if(inputRef.current != null) {
-                  inputRef.current.click();
-                }
-              }}>Open</Button>
-            </span>
-            <span className='Button'>
-              <Button className='Button' variant='contained' onClick={uploadHandler} disabled={file === undefined}>Upload</Button>
-            </span>
-            <span className='DelButton'>
-              <IconButton aria-label='delete' color='primary' onClick={() => {
-                setFile(undefined);
-              }}>
-                <Delete/>
-              </IconButton>
-            </span>
-          </div>
+          <ControlPanel file={file} openHandler={()=>openHandler()} uploadHandler={()=>uploadHandler()} clearHandler={()=>clearHandler()} />
         </Box>
       </div>
+
     </div>
   );
 }
--- src/Components/ControlPanel/ControlPanel.tsx
+++ src/Components/ControlPanel/ControlPanel.tsx
@@ -1,8 +1,38 @@
 import React, {useState, useRef} from 'react';
+import DisplayBox from '../DisplayBox/DisplayBox';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import { Delete } from '@mui/icons-material';
+import './ControlPanel.css'
 
-interface ICBProps {
-
+interface IProps {
+    file: File | undefined;
+    openHandler: () => void;
+    uploadHandler: () => void;
+    clearHandler: () => void;
 }
 
-function ControlButton(props: ICBProps) {
+function ControlPanel(props: IProps) {
+    const file = props.file;
+    const openHandler = props.openHandler;
+    const uploadHandler = props.uploadHandler;
+    const clearHandler = props.clearHandler;
+    return (
+        <div className='ManuButtons'>
+            <DisplayBox file={file} />
+            <span className='Button'>
+                <Button variant='contained' onClick={openHandler}>Open</Button>
+            </span>
+            <span className='Button'>
+                <Button className='Button' variant='contained' onClick={uploadHandler} disabled={file === undefined}>Upload</Button>
+            </span>
+            <span className='DelButton'>
+                <IconButton aria-label='delete' color='primary' onClick={clearHandler}>
+                    <Delete/>
+                </IconButton>
+            </span>
+        </div>
+    )
 }
+
+export default ControlPanel;
